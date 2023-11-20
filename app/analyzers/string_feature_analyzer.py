9ecc52efb1228f24ee415ff94683e9ee79963bea
liuchengyue <17554334529@163.com>
2023-11-20 16:35:29 +0800

    add logs

--- app/analyzers/string_feature_analyzer.py
+++ app/analyzers/string_feature_analyzer.py
@@ -75,10 +75,15 @@ def walk_raw_features():
     # 读取文件路径
     logger.info(f"start walk feature_file_paths")
     feature_file_paths = []
+    count = 0
     for root, dirs, files in os.walk(PROCESSED_FILE_FEATURE_DIR):
         for f in files:
             f_path = os.path.join(root, f)
-            feature_file_paths.append(f_path)
+            if f_path.endswith('.json'):
+                count += 1
+                if count % 1000 == 0:
+                    logger.info(f"walk progress: {count}")
+                feature_file_paths.append(f_path)
     logger.info(f"walk feature_file_paths finished, num: {len(feature_file_paths)}")
 
     # 多进程统计
@@ -92,5 +97,3 @@ def walk_raw_features():
 
     pool.close()
     pool.join()
-
-
